32802b61f5c3d4d3206789df5f22539b9dfeb5b8
ADITYA KANDALKAR <138191544+AdityaAK67@users.noreply.github.com>
2025-03-15 21:10:25 +0530

    Update touch.py

--- touch.py
+++ touch.py
@@ -1,44 +1,20 @@
-# import RPi.GPIO as GPIO
-# import time
-
-# TOUCH_SENSOR_PIN = 17  # GPIO17 (Pin 11)
-
-# GPIO.setmode(GPIO.BCM)  # Use Broadcom pin numbering
-# GPIO.setup(TOUCH_SENSOR_PIN, GPIO.IN)  # Set as input
-
-# print("Touch sensor test started. Press the sensor...")
-
-# try:
-#     while True:
-#         if GPIO.input(TOUCH_SENSOR_PIN) == GPIO.HIGH:
-#             print("Touch detected!")
-#         time.sleep(0.1)  # Short delay to avoid excessive logging
-
-# except KeyboardInterrupt:
-#     print("\nTest stopped.")
-#     GPIO.cleanup()  # Reset GPIO settings
-
-
-import pigpio
+import RPi.GPIO as GPIO
 import time
 
 TOUCH_SENSOR_PIN = 17  # GPIO17 (Pin 11)
-pi = pigpio.pi()
 
-if not pi.connected:
-    print("Error: pigpio daemon not running!")
-    exit()
-
-pi.set_mode(TOUCH_SENSOR_PIN, pigpio.INPUT)
+# Setup
+GPIO.setmode(GPIO.BCM)  # Use Broadcom pin numbering
+GPIO.setup(TOUCH_SENSOR_PIN, GPIO.IN)  # Set pin as input
 
 print("Touch sensor test started. Press the sensor...")
 
 try:
     while True:
-        if pi.read(TOUCH_SENSOR_PIN):  # Read GPIO state
+        if GPIO.input(TOUCH_SENSOR_PIN) == GPIO.HIGH:
             print("Touch detected!")
-        time.sleep(0.1)  
+        time.sleep(0.1)  # Short delay to avoid excessive logging
 
 except KeyboardInterrupt:
     print("\nTest stopped.")
-    pi.stop()  # Clean up pigpio connection
+    GPIO.cleanup()  # Reset GPIO settings
